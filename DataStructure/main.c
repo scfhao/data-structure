614927c993a8d9d175e46bee28166a87ce080afa
scfhao <scfhao@126.com>
2022-03-13 15:59:34 +0800

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by Zhang Hao on 2022/3/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
